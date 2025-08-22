1c2481d698fee4c52c11fe6de0f417e9b317b448
Jack <32371937+jackzhxng@users.noreply.github.com>
2025-08-22 14:21:37 -0700

    Add tekken to the tokenize tool (#119)

--- examples/tokenize_tool/main.cpp
+++ examples/tokenize_tool/main.cpp
@@ -22,6 +22,7 @@
 #include "hf_tokenizer.h"
 #include "sentencepiece.h"
 #include "tiktoken.h"
+#include "tekken.h"
 
 using namespace tokenizers;
 
@@ -64,6 +65,8 @@ int main(int argc, char* argv[]) {
     tok_ptr.reset(new Tiktoken());
   } else if (tokenizer_type == "hf_tokenizer") {
     tok_ptr.reset(new HFTokenizer());
+  } else if (tokenizer_type == "tekken") {
+    tok_ptr.reset(new Tekken());
   } else {
     std::stringstream ss;
     ss << "ERROR: Invalid tokenizer type: " << tokenizer_type << std::endl
