deb893d4acb62a5dd850fca8ec4d94e8cead2766
Jacob Szwejbka <jakeszwe@fb.com>
2025-10-18 12:34:00 -0700

    normalizer fix

--- src/normalizer.cpp
+++ src/normalizer.cpp
@@ -49,7 +49,7 @@ Normalizer::Ptr NormalizerConfig::create() const {
     return Normalizer::Ptr(new ReplaceNormalizer(*pattern, *content));
   }
   if (type == "Sequence") {
-    if (!normalizers or normalizers->empty()) {
+    if (!normalizers || normalizers->empty()) {
       throw std::runtime_error(
           "Missing normalizers for Normalizer of type Sequence");
     }
